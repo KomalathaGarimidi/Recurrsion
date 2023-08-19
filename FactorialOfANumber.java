2ca4ede5eaa3591bcc570a3792421c26c82b008b
komlatha <garimidikomalatha62@gmail.com>
2023-08-19 22:29:25 +0530

    Find the factorial of a number using recurssion

--- FactorialOfANumber.java
+++ FactorialOfANumber.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class FactorialOfANumber {
+import java.util.Scanner;
+
+public class FactorialOfANumber {
+    public static int factorial(int N){
+        if(N==1 || N==0 ){
+            return 1;
+        }
+        return N*factorial(N-1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int N=scanner.nextInt();
+        System.out.println(FactorialOfANumber.factorial(N));
+    }
 }
